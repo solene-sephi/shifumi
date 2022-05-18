fe94f1991f23e5b0ee8c261347594871332b0542
Solene Brunel <solene.brunel@free.fr>
2022-05-18 11:36:28 +0200

    ajout d'une 2ème version du js + comparaison des armes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,20 @@
 -> Le joueur clique sur le bouton
     1- Récupérer l'arme choisie par le joueur
     2- Générer aléatoirement une arme pour l'ordinateur
-    3- Comparer les deux armes 
-    4- Déterminer qui est le gagnant
-    5- Afficher le résultat
+    3- Comparer les deux armes pour déterminer le gagnat
+            joueur pierre / ordi pierre = égalité
+            joueur feuille / ordi feuille = égalité
+            joueur ciseaux / ordi ciseaux = égalité
 
+            joueur pierre / ordi feuille = défaite
+            joueur feuille / ordi ciseaux = défaite
+            joueur ciseaux / ordi pierre = défaite
+
+            joueur pierre / ordi ciseaux = victoire
+            joueur feuille / ordi pierre = victoire
+            joueur ciseaux / ordi feuille = victoire
+
+    5- Afficher le résultat
 */
 
 // Déclaration des variables pour les armes du joueur
@@ -37,14 +47,21 @@ scissorsPlayer.on('click', () => {
 
 // Au clic sur le bouton de jeu, on récupère l'élément qui a la class 'weaponSelected'
 $("#fightButton").on('click', () => {
-    const weaponSelected = $('li').hasClass("weaponSelectedStyle");
-    console.log(weaponSelected);
+    let playerWeapon = $('li.weaponSelectedStyle');
+    console.log(playerWeapon);
 
     // Déclaration d'un tableau pour les armes de l'ordinateur
     const computerWeapons = ['rock', 'paper', 'scissors'];
     // Choix aléatoire une arme dans le tableau pour l'ordinateur
-    let randomWeapon = computerWeapons[Math.floor(Math.random()*computerWeapons.length)]
+    let randomWeapon = computerWeapons[Math.floor(Math.random() * computerWeapons.length)]
     console.log(randomWeapon);
 
     // Comparaison des armes 
+    // Si arme similaire, égalité
+    if ((($("#rockHand").hasClass("weaponSelectedStyle")) && (randomWeapon === 'rock')) || (($("#paperHand").hasClass("weaponSelectedStyle")) && (randomWeapon === 'paper')) || (($("#scissorsHand").hasClass("weaponSelectedStyle")) && (randomWeapon === 'scissors'))) {
+        console.log('égalité');
+    }
+    else if ((($("#rockHand").hasClass("weaponSelectedStyle")) && (randomWeapon === 'paper')) || (($("#paperHand").hasClass("weaponSelectedStyle")) && (randomWeapon === 'scissors')) || (($("#scissorsHand").hasClass("weaponSelectedStyle")) && (randomWeapon === 'rock'))) {
+        console.log('défaite');
+    }
 })
---
+++ assets/js/script1.js
@@ -0,0 +1,67 @@
+/*
+-> Le joueur choisi une arme 
+    1- Indiquer visuellement quelle arme est choisie
+
+-> Le joueur clique sur le bouton
+    1- Récupérer l'arme choisie par le joueur
+    2- Générer aléatoirement une arme pour l'ordinateur
+    3- Comparer les deux armes pour déterminer le gagnant
+            joueur pierre / ordi pierre = égalité
+            joueur feuille / ordi feuille = égalité
+            joueur ciseaux / ordi ciseaux = égalité
+
+            joueur pierre / ordi feuille = défaite
+            joueur feuille / ordi ciseaux = défaite
+            joueur ciseaux / ordi pierre = défaite
+
+            joueur pierre / ordi ciseaux = victoire
+            joueur feuille / ordi pierre = victoire
+            joueur ciseaux / ordi feuille = victoire
+    4- Afficher le résultat aléatoire de l'arme de l'ordinateur
+    5- Afficher le résultat du jeu : victoire, défaite, égalité
+*/
+
+// Au click sur une des armes, une class 'weaponSelected' affichant une border est ajoutée à cette arme et retirée des autres armes
+$("#rockHand").on('click', () => {
+    $("#rockHand").addClass("weaponSelectedStyle");
+    $("#paperHand").removeClass("weaponSelectedStyle");
+    $("#scissorsHand").removeClass("weaponSelectedStyle");
+})
+
+$("#paperHand").on('click', () => {
+    $("#paperHand").addClass("weaponSelectedStyle");
+    $("#rockHand").removeClass("weaponSelectedStyle");
+    $("#scissorsHand").removeClass("weaponSelectedStyle");
+})
+
+$("#scissorsHand").on('click', () => {
+    $("#scissorsHand").addClass("weaponSelectedStyle");
+    $("#rockHand").removeClass("weaponSelectedStyle");
+    $("#paperHand").removeClass("weaponSelectedStyle");
+})
+
+// Au clic sur le bouton de jeu,
+$("#fightButton").on('click', () => {
+    // Déclaration des variables pour l'arme choisie par le joueur (donc qui a la class 'weaponSelected')
+    const rockPlayer = $("#rockHand").hasClass("weaponSelectedStyle");
+    const paperPlayer = $("#paperHand").hasClass("weaponSelectedStyle");
+    const scissorsPlayer = $("#scissorsHand").hasClass("weaponSelectedStyle");
+    
+    // Déclaration d'un tableau qui liste les armes disponibles pour l'ordinateur
+    const computerWeapons = ['rock', 'paper', 'scissors'];
+    // Choix aléatoire une arme dans le tableau pour l'ordinateur
+    let randomWeapon = computerWeapons[Math.floor(Math.random() * computerWeapons.length)]
+    console.log(randomWeapon);
+
+    // Comparaison des armes du joueur et de l'ordinateur
+    // Si armes similaires, égalité
+    if (((rockPlayer === true) && (randomWeapon === 'rock')) || ((paperPlayer === true) && (randomWeapon === 'paper')) || ((scissorsPlayer=== true) && (randomWeapon === 'scissors'))) {
+        console.log('égalité');
+    }
+    else if (((rockPlayer === true) && (randomWeapon === 'paper')) || ((paperPlayer === true) && (randomWeapon === 'scissors')) || ((scissorsPlayer === true) && (randomWeapon === 'rock'))) {
+        console.log('défaite');
+    }
+    else {
+        console.log('victoire')
+    }
+})
